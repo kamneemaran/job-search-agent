0c6150e9481a8181e578340d44660f220f649b70
Kamnee Maran <kamneemaran45@gmail.com>
2026-06-22 01:18:33 +0530

    Fix US/CAN urls

--- us_canada_companies.py
+++ us_canada_companies.py
@@ -5,8 +5,6 @@ US_CANADA_JOB_SOURCES = [
     {"name": "Prime Intellect", "url": "https://jobs.ashbyhq.com/PrimeIntellect", "region": "US/Canada", "type": "company", "ats": "ashby", "ats_slug": "PrimeIntellect"},
     {"name": "Verneek", "url": "https://apply.workable.com/verneek", "region": "US/Canada", "type": "company", "ats": "workable"},
 
-    {"name": "Emirates Group IT", "url": "https://www.emiratesgroupcareers.com", "region": "US/Canada", "type": "company", "playwright": True},
-    {"name": "Etisalat Digital", "url": "https://iaayey.fa.ocs.oraclecloud26.com/hcmUI/CandidateExperience/en/sites/CX_1/jobs", "region": "US/Canada", "type": "company", "playwright": True},
     {"name": "Latitude AI", "url": "https://lat.ai/careers#open-roles", "region": "US/Canada", "type": "company", "playwright": True},
     {"name": "RapDev", "url": "https://www.rapdev.io/company/careers#positions", "region": "US/Canada", "type": "company", "playwright": True},
     {"name": "ULINE", "url": "https://www.uline.jobs/JobSearchResults?culture=en", "region": "US/Canada", "type": "company", "playwright": True},
