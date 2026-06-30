88226c564f64313d8eb327a4e11b778ebe027eae
kamneemaran <kamneemaran45@gmail.com>
2026-06-30 23:18:30 +0530

    Update Moment URL with IT category filter

--- eu_companies.py
+++ eu_companies.py
@@ -858,7 +858,7 @@ EU_JOB_SOURCES = [
     {"name": "Bravura", "url": "https://www.bravura.se/jobb/", "region": "EU", "type": "company", "playwright": True},
     {"name": "Incluso", "url": "https://openings.incluso.se/jobs", "region": "EU", "type": "company", "playwright": True},
     {"name": "Workindenmark", "url": "https://www.workindenmark.dk/", "region": "EU", "type": "company", "playwright": True},
-    {"name": "Moment", "url": "https://www.moment.dk/", "region": "EU", "type": "company", "playwright": True},
+    {"name": "Moment", "url": "https://www.moment.dk/en/find-job/?categories=26", "region": "EU", "type": "company", "playwright": True},
     {"name": "Hartmanns", "url": "https://actief-hartmanns.dk/", "region": "EU", "type": "company", "playwright": True},
     {"name": "Temp-Team Denmark", "url": "https://temp-team.dk/", "region": "EU", "type": "company", "playwright": True},
     {"name": "Academic Work Denmark", "url": "https://www.academicwork.dk/", "region": "EU", "type": "company", "playwright": True},
