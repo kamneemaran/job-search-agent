f341b814d4ff12d5865b61b9bcbd7918b95a540c
kamneemaran <kamneemaran45@gmail.com>
2026-07-30 17:47:41 +0530

    Update LINE (Yahoo Japan) URL to recruit career page

--- apac_companies.py
+++ apac_companies.py
@@ -49,7 +49,7 @@ APAC_JOB_SOURCES = [
     {"name": "Rakuten", "url": "https://japan-job-en.rakuten.careers/engineering-en", "region": "APAC", "type": "company", "playwright": True},
     {"name": "Sony", "url": "https://www.sony.com/en/SonyInfo/careers/", "region": "APAC", "type": "company", "playwright": True},
     {"name": "Mercari", "url": "https://careers.mercari.com/jobs/?job_category=jc-engineering+engineering+corporate-engineering+security-engineering", "region": "APAC", "type": "company", "playwright": True},
-    {"name": "LINE (Yahoo Japan)", "url": "https://careers.lycorp.co.jp/en/", "region": "APAC", "type": "company", "playwright": True},
+    {"name": "LINE (Yahoo Japan)", "url": "https://www.lycorp.co.jp/en/recruit/career/job-categories/#all", "region": "APAC", "type": "company", "playwright": True},
     {"name": "Preferred Networks", "url": "https://www.preferred-networks.jp/en/careers", "region": "APAC", "type": "company", "playwright": True},
     {"name": "Sansan", "url": "https://jp.careers.sansan.com/en/", "region": "APAC", "type": "company", "playwright": True},
     {"name": "SmartHR", "url": "https://hrmos.co/pages/smarthr/jobs", "region": "APAC", "type": "company", "playwright": True},
