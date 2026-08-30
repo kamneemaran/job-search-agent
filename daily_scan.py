0f25ac60ae8c2c463e0989cd4553e00d86ce5443
kamnee maran <kamnee.maran@phonepe.com>
2026-08-30 19:07:40 +0530

    Fix: Add Xing and EURES to boards-eu batch for comprehensive EU SAP MM job search

--- daily_scan.py
+++ daily_scan.py
@@ -9619,8 +9619,6 @@ def main():
             ("LinkedInFR", search_linkedin_fr),
             ("LinkedInIT", search_linkedin_it),
             ("VDAB", search_vdab),
-            ("Xing", search_xing),
-            ("EURES", search_eures),
             ("MichaelPage", search_michaelpage),
             ("Hays", search_hays),
             ("Randstad", search_randstad),
@@ -9634,8 +9632,10 @@ def main():
             ("RabotaBG", search_rabota_bg),
             ("JobIndexDK", search_jobindex_dk),
             ("StepStoneDK", search_stepstone_dk),
-            ("StepStoneNO", search_stepstone_no),
-        ]
+             ("StepStoneNO", search_stepstone_no),
+             ("Xing", search_xing),
+             ("EURES", search_eures),
+         ]
     elif args.batch == "boards-remote":
         _get_browser()
         board_scrapers = [
@@ -9662,9 +9662,9 @@ def main():
         au_boards = {"Seek", "Jora"}
         eu_boards = {"Xing", "JobsCh", "JobsinGermany", "WorkinFinland", "EURES"}
         remote_boards = {"WeWorkRemotely", "Remotive", "ArcDev", "RemoteOK", "SkipTheDrive", "WorkingNomads", "Jobspresso", "Arbeitnow", "EnglishJobSearch", "Bulldogjob", "VisaSponsor", "Incluso", "Crossover", "NoDesk", "Workew", "Kelly"}
-         single_run_boards = {"NetEmpregos", "SAPOEmprego", "Infoempleo", "Bundesagentur", "IamExpat", "WorkInLux", "IndeedNL", "IndeedGR", "IndeedFI", "WelcomeToNL", "TogetherAbroad", "StepStone", "Adzuna", "Intermediair", "NationaleVacaturebank", "VDAB", "Xing", "EURES", "MichaelPage", "Hays", "Randstad", "RobertHalf", "MichaelPagePL", "NoFluffJobs", "eJobsRO", "JobsCZ", "ProfessionHU", "MichaelPagePT", "RabotaBG", "JobIndexDK", "StepStoneDK", "StepStoneNO"} | remote_boards
-        pw_names = {"SAPOEmprego", "Bundesagentur", "IamExpat", "WorkInLux", "IndeedNL", "IndeedGR", "IndeedFI", "StepStone", "Freelancermap", "Intermediair", "NationaleVacaturebank", "WorkingNomads", "Jobspresso", "Bulldogjob", "Crossover", "Kelly", "VDAB", "Xing", "EURES", "MichaelPage", "Hays", "Randstad", "RobertHalf", "MichaelPagePL", "NoFluffJobs", "eJobsRO", "JobsCZ", "ProfessionHU", "MichaelPagePT", "RabotaBG", "JobIndexDK", "StepStoneDK", "StepStoneNO"}
-        static_boards = {"SAPOEmprego", "Infoempleo", "IamExpat", "WorkInLux", "TogetherAbroad", "VisaSponsor", "WorkingNomads", "Jobspresso", "NoDesk", "VDAB", "Xing", "EURES", "MichaelPage", "Hays", "Randstad", "RobertHalf", "MichaelPagePL", "NoFluffJobs", "eJobsRO", "JobsCZ", "ProfessionHU", "MichaelPagePT", "RabotaBG", "JobIndexDK", "StepStoneDK", "StepStoneNO"}
+        single_run_boards = {"NetEmpregos", "SAPOEmprego", "Infoempleo", "Bundesagentur", "IamExpat", "WorkInLux", "IndeedNL", "IndeedGR", "IndeedFI", "WelcomeToNL", "TogetherAbroad", "StepStone", "Adzuna", "Intermediair", "NationaleVacaturebank", "VDAB", "MichaelPage", "Hays", "Randstad", "RobertHalf", "MichaelPagePL", "NoFluffJobs", "eJobsRO", "JobsCZ", "ProfessionHU", "MichaelPagePT", "RabotaBG", "JobIndexDK", "StepStoneDK", "StepStoneNO"} | remote_boards
+        pw_names = {"SAPOEmprego", "Bundesagentur", "IamExpat", "WorkInLux", "IndeedNL", "IndeedGR", "IndeedFI", "StepStone", "Freelancermap", "Intermediair", "NationaleVacaturebank", "WorkingNomads", "Jobspresso", "Bulldogjob", "Crossover", "Kelly", "VDAB", "MichaelPage", "Hays", "Randstad", "RobertHalf", "MichaelPagePL", "NoFluffJobs", "eJobsRO", "JobsCZ", "ProfessionHU", "MichaelPagePT", "RabotaBG", "JobIndexDK", "StepStoneDK", "StepStoneNO"}
+        static_boards = {"SAPOEmprego", "Infoempleo", "IamExpat", "WorkInLux", "TogetherAbroad", "VisaSponsor", "WorkingNomads", "Jobspresso", "NoDesk", "VDAB", "MichaelPage", "Hays", "Randstad", "RobertHalf", "MichaelPagePL", "NoFluffJobs", "eJobsRO", "JobsCZ", "ProfessionHU", "MichaelPagePT", "RabotaBG", "JobIndexDK", "StepStoneDK", "StepStoneNO"}
 
         def _process_board(board_name, board_fn):
             collected = []
